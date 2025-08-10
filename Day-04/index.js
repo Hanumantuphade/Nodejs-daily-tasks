ebebb7477cb7a354b798b1f1300d783c18198bae
Hanumantuphade <hanumantuphade63@gmail.com>
2025-08-10 17:38:16 +0530

    Refactor middleware logging and add initial setup for form handling, sessions, and cookies

--- Day-04/index.js
+++ Day-04/index.js
@@ -1,34 +1,27 @@
-const { log } = require("console");
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to log request details
 app.use((req, res, next) => {
-  log("run frist middleware");
+ console.log("run frist middleware");
   next(); // Call the next middleware or route handler
 });
 
 app.use((req, res, next) => {
-  log("run second middleware");
+  console.log("run second middleware");
   next(); // Call the next middleware or route handler
 });
 
 app.get("/", (req, res) => {
   res.send("hello world");
-  next();
 });
 
 app.get("/about", (req, res) => {
   res.send("This is about page");
 });
-app.get("/contact", (req, res,next) => {
-  return next(new Error("This is a contact page error"));
-});
-app.use((err,req,res,next)=>{
-  console.error(err.stack); // Log the error stack trace
-  res.status(500).send("Something went wrong!"); // Send a generic error response
-})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ Day-05/Form Handling, Sessions & Cookies.js
@@ -0,0 +1,15 @@
+// Form Handling :-
+
+//-	when a user submits a doem (like login or contact), the server needs to receive and process the form data.
+//-	Express.js provides middleware like `express.urlencoded()` to parse URL-encoded data and `express.json()` for JSON data.
+
+
+// cookies :-
+//-	cookies are small pieces of data stored on the client-side by the browser.
+//-	They are used to remember user preferences, sessions, and other information.
+//-	Express.js provides the `cookie-parser` middleware to handle cookies easily.
+
+// Sessions :-
+//-	sessions are used to store user-specific data on the server-side.
+//-	They are often used for authentication and maintaining user state.
+
